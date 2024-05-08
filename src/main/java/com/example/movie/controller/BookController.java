c18d79b8b6fbf3f0d1df1039101a352e6f8586f8
eueo8259 <eueo8259@gmail.com>
2024-05-08 15:29:05 +0900

    #movie 서비스 통합 및 Book 서비스 통합

--- src/main/java/com/example/movie/controller/BookController.java
+++ src/main/java/com/example/movie/controller/BookController.java
@@ -1,16 +1,17 @@
 package com.example.movie.controller;
 
 import com.example.movie.dto.LocationDto;
+import com.example.movie.dto.MoviesDto;
 import com.example.movie.dto.SeatDto;
+import com.example.movie.dto.TicketDto;
+import com.example.movie.service.BookService;
 import com.example.movie.service.LocationService;
-import com.example.movie.service.SeatService;
+import com.example.movie.service.MovieService;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -22,12 +23,15 @@ import java.util.List;
 public class BookController {
 
     private final LocationService locationService;
-    private final SeatService seatService;
+    private final MovieService movieService;
+    private final BookService bookService;
 
-    public BookController(LocationService locationService, SeatService seatService) {
+    public BookController(LocationService locationService, MovieService movieService, BookService bookService) {
         this.locationService = locationService;
-        this.seatService = seatService;
+        this.movieService = movieService;
+        this.bookService = bookService;
     }
+
 
     @GetMapping("theater")
     public String selectTheaterView(Model model){
@@ -40,15 +44,39 @@ public class BookController {
         model.addAttribute("dateList", dateList);
 
         List<LocationDto> locationDtoList = locationService.findAll();
-        log.info("================================================");
-        log.info(locationDtoList.stream().toList().toString());
-
         model.addAttribute("locationDtoList", locationDtoList);
-        return "/articles/book/select_theater";
+
+        List<MoviesDto> moviesDtoList = movieService.findAll();
+        model.addAttribute("moviesDtoList", moviesDtoList);
+        return "/book/select_theater";
+    }
+
+    @PostMapping("/theater")
+    public String selectSeat(
+            @RequestParam("movie") Long movieNo,
+            @RequestParam("location") Long locationNo,
+            @RequestParam("date") LocalDate date,
+            HttpSession session){
+        log.info("===============================");
+        log.info("===============================");
+        log.info("===============================");
+        log.info(String.valueOf(movieNo));
+        log.info(String.valueOf(locationNo));
+        log.info(String.valueOf(date));
+        session.setAttribute("movieId", movieNo);
+        session.setAttribute("locationId", locationNo);
+        session.setAttribute("date", date);
+
+        return "redirect:/cnema/seat";
     }
 
     @GetMapping("/seat")
-    public String selectSeatView(Model model){
+    public String selectSeatView(Model model, HttpSession session){
+        Long movieNo = (Long) session.getAttribute("movieNo");
+        Long locationNo = (Long) session.getAttribute("locationNo");
+        LocalDate date = (LocalDate) session.getAttribute("date");
+
+        List<Long> seatId = bookService.findSeatIdsByMovieNoAndLocationNoAndBookDate(movieNo, locationNo, date);
 
 //        int[] row = new int[6];
 //        int[] column = new int[8];
@@ -58,15 +86,12 @@ public class BookController {
 //        for (int j = 0; j < column.length; j++) {
 //            column[j] = j;
 //        }  좌석의 크기가 커지면 for문을 크지 않다면 아래처럼 직접 적어주는게 오히려 유지보수에 좋을 수 있다.
-
         int[] row = {0,1,2,3,4,5};
         int[] column = {0,1,2,3,4,5,6,7};
         model.addAttribute("row", row);
         model.addAttribute("column", column);
         model.addAttribute("seatDto", new SeatDto());
 
-        return "/articles/book/select_seat";
+        return "/book/select_seat";
     }
-
-
 }
--- src/main/java/com/example/movie/dto/SeatDto.java
+++ src/main/java/com/example/movie/dto/SeatDto.java
@@ -1,4 +1,16 @@
 package com.example.movie.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class SeatDto {
+    private Long seatId;
+    private int seatRowNo;
+    private int seatColumnNo;
 }
