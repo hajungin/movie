6b152130e7799756aa0c27be87f2a11b1bf22e7a
hajungin <hji000426@gmail.com>
2024-05-09 12:04:49 +0900

    # 정인 관리자 2차 완성

---
+++ src/main/resources/static/js/movie_check.js
@@ -0,0 +1,32 @@
+function check() {
+    var movieTitle = document.getElementById("movieTitle").value.trim();
+    var movieDate = document.getElementById("movieDate").value.trim();
+    var movieRate = document.getElementById("movieRate").value;
+    var goodPointAvg = document.getElementById("goodPointAvg").value;
+
+
+    if (movieTitle.length == 0) {
+        alert("아이디가 입력되지 않았습니다.");
+        document.getElementById("movieTitle").focus();
+        return false;
+    }
+    if (movieDate.length == 0) {
+        alert("이름이 입력되지 않았습니다.");
+        document.getElementById("movieDate").focus();
+        return false;
+    }
+    if (movieRate.length == 0) {
+        alert("비밀번호가 입력되지 않았습니다.");
+        document.getElementById("movieRate").focus();
+        return false;
+    }
+    if (movieRate.length == 0) {
+            alert("비밀번호가 입력되지 않았습니다.");
+            document.getElementById("movieRate").focus();
+            return false;
+        }
+
+    alert("입력이 완료되었습니다.");
+    document.getElementById("frm").submit();
+    return true;
+}
