88648c5ea70e772bf161dd5e022e3326363144f0
hajungin <hji000426@gmail.com>
2024-05-09 09:31:30 +0900

    # 정인 Movies 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,9 +7,9 @@ VALUES
 ('1978-12-30', 'bob_johnson', 'bobpass', NULL, 'Bob Johnson', 'USER', 'bob@example.com');
 INSERT INTO movies (good_point_avg, movie_date, movie_rate, movie_title)
 VALUES
-(4.5, '2024-05-01 12:00:00', 'PG-13', 'The Avengers'),
-(3.8, '2024-05-02 13:00:00', 'PG', 'Toy Story'),
-(4.2, '2024-05-03 14:00:00', 'PG-13', 'Jurassic Park');
+(4.5, '20240501', 'PG-13', 'The Avengers'),
+(3.8, '20240502', 'PG', 'Toy Story'),
+(4.2, '20240503', 'PG-13', 'Jurassic Park');
 
 insert into location (location_name) values ('서울');
 insert into location (location_name) values ('일산');
